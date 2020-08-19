095de34c74602817e6382e5d637ddcd525a52355
lzj4103@126.com <liuzijing3344>
2020-08-19 18:02:55 +0800

    1

--- wxapp/cloudfunctions/addTask/index.js
+++ wxapp/cloudfunctions/addTask/index.js
@@ -7,7 +7,26 @@ const db = cloud.database();
 
 const _ = db.command;
 
-每天零点执行
+//每天零点执行
+
+function getTime() {
+	var date = new Date();
+	var year = date.getFullYear();
+  var mon = date.getMonth() + 1;
+
+  mon = mon[1]?mon:`0${mon}`;
+
+  var day = date.getDate();
+  
+  // day = day[1]?day:`0${day}`;
+
+	var hours = date.getHours();
+	var minu = date.getMinutes();
+	var sec = date.getSeconds();
+
+	return  `${year}-${mon}-${day[1]}`;
+}
+
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -19,11 +38,9 @@ exports.main = async (event, context) => {
     {
       repeat_week: new Date().getDay()        // 或者发送的星期是今天的
     }
-  ])).get()
+  ])).get();
 
-  // let res = await db.collection('MedicationInfo').where({
-  //   "cycle": 1
-  // }).get();
 
-  return res;
+
+  return getTime();
 }
