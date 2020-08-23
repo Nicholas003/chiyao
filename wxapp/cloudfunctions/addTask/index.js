6013915925ac0158096aa4865b288e6d64576d59
刘紫旌 <lzj4103@126.com>
2020-08-23 15:41:38 +0800

    1

--- wxapp/cloudfunctions/addTask/index.js
+++ wxapp/cloudfunctions/addTask/index.js
@@ -15,7 +15,7 @@ const _ = db.command;
 //   let now_arr = nowdate.split('/');
 //   return `${now_arr[2]}-${now_arr[0]}-${now_arr[1]}`;
 // }
-function get_time(in_time){
+function get_time(in_time) {
   let date = new Date();
 
   let time = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${in_time}:00`;
@@ -26,31 +26,38 @@ function get_time(in_time){
 // 云函数入口函数
 exports.main = async (event, context) => {
 
-  let {data} = await db.collection('MedicationInfo').where(_.or([
-    {
-      cycle: 0                                 //获取每天都需要推送消息的的
+  let {
+    data
+  } = await db.collection('MedicationInfo').where(_.or([{
+      cycle: 0 //获取每天都需要推送消息的的
     },
     {
-      repeat_week: new Date().getDay()        // 或者发送的星期是今天的
+      repeat_week: new Date().getDay() // 或者发送的星期是今天的
     }
-  ])).get();
+  ])).where({
+    on: true
+  }).get();
 
-  for(let i=0;i<data.length;i++){
-    let {medication_time} = data[i];
+  for (let i = 0; i < data.length; i++) {
+    let {
+      medication_time
+    } = data[i];
     // return data;
-    for(let e=0;e<medication_time.length;e++){
+    for (let e = 0; e < medication_time.length; e++) {
       let add_data = {
-        push_time:get_time(medication_time[e]),                             //需要发送的时间
-        _openid:data[i].userInfo.openId,
-        medication_id:data[i]._id,                                          //药物信息的id
-        name:data[i].name,
-        cons:data[i].cons||'',
-        is_push:0,      //0未推送 1再推送队列中 2已推送 3推送失败  如果当前时间大于需要推送的时间表明已经过期 所以设置为已推送
-        confirm:0,                                                      //是否完成  0未完成  1已完成
-        created_at:new Date().getTime(),
-        updata_at:new Date().getTime(),
+        push_time: get_time(medication_time[e]), //需要发送的时间
+        _openid: data[i].userInfo.openId,
+        medication_id: data[i]._id, //药物信息的id
+        name: data[i].name,
+        cons: data[i].cons || '',
+        is_push: 0, //0未推送 1再推送队列中 2已推送 3推送失败  如果当前时间大于需要推送的时间表明已经过期 所以设置为已推送
+        confirm: 0, //是否完成  0未完成  1已完成
+        created_at: new Date().getTime(),
+        updata_at: new Date().getTime(),
       }
-      await db.collection('PublishQueue').add({data:add_data});
+      await db.collection('PublishQueue').add({
+        data: add_data
+      });
       // return add_data;
     }
   }
--- wxapp/miniprogram/pages/remind/remind.js
+++ wxapp/miniprogram/pages/remind/remind.js
@@ -6,19 +6,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    week:app.week
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
-    let {
-			data
-    } = await app.cloud.call('getRemind');
+    let {data} = await app.cloud.call('getRemind');
+
+    for(let i=0;i<data.length;i++){
+      let end_time = app.formatTime(new Date(data[i].end_time)).split(' ')[0];
+      console.log(end_time);
+      data[i].end_time = end_time;
+    }
+
     this.setData({
       data
-    })
+    });
+    wx.stopPullDownRefresh();
+  },
+  async on_change({currentTarget:{dataset:{id}},detail:{value}}){
+    // console.log(id,value)
+    const _ = app.db.command;
+    let res = await app.db.collection('MedicationInfo').doc(id).update({
+      data: {
+        on:_.set(value)
+      }
+    });
+    console.log(res)
   },
 
   /**
@@ -53,7 +69,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.onLoad();
   },
 
   /**
